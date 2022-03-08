f8c5d11f8bb384128ed692bbc98674910fb98a0d
Lee Changmoo <lcm8543@gmail.com>
2022-03-08 16:42:02 +0900

    merge front

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MainPage from "./pages/Mainpage";
+import ReviewDetail from "./pages/ReviewDetail/ReviewDetail";
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      {/* <MainPage /> */}
+      <ReviewDetail />
     </div>
   );
 }
--- src/pages/ReviewDetail/ReviewDetail.js
+++ src/pages/ReviewDetail/ReviewDetail.js
@@ -1,14 +1,20 @@
 import styles from "./ReviewDetail.module.scss";
 import ReviewDetailHeader from "../../components/ReviewDetail/ReviewDetailHeader";
 import ReviewContent from "../../components/ReviewDetail/ReviewContent";
+import Comments from "../../components/Comments/Comments";
+import { useSelector } from "react-redux";
 
 const ReviewDetail = () => {
+  let data = useSelector((state) => state);
+  console.log(data);
+
   return (
     <div className={styles.container}>
       <div>
         {/* contentHeader */}
         <ReviewDetailHeader />
         <ReviewContent />
+        <Comments comments={data.contents.data[0].comment} />
       </div>
     </div>
   );
