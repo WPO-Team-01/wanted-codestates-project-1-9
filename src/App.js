48800787ab812fbcbfb5896b24cd334b12ea8bb6
Sophie <jihye340@gmail.com>
2022-03-08 16:39:51 +0900

    Feature : add data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import ReviewContent from "./components/ReviewDetail/ReviewContent";
+import ReviewDetail from "./pages/ReviewDetail/ReviewDetail";
 import MainPage from "./pages/Mainpage";
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      {/* <MainPage /> */}
+      <ReviewDetail />
     </div>
   );
 }
