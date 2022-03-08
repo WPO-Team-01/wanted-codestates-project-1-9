08fa5a20828e58308d2d99b1683a7de5d9c7ad90
Sophie <jihye340@gmail.com>
2022-03-08 22:39:57 +0900

    Feature : data fetch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import "./App.css";
 import ReviewContent from "./components/ReviewDetail/ReviewContent";
 import ReviewDetail from "./pages/ReviewDetail/ReviewDetail";
 import MainPage from "./pages/Mainpage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      {/* <MainPage /> */}
-      <ReviewDetail />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/:id" element={<ReviewDetail />}></Route>
+          {/* <MainPage />
+          <ReviewDetail /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/ReviewDetail/ReviewDetailHeader.js
+++ src/components/ReviewDetail/ReviewDetailHeader.js
@@ -1,13 +1,18 @@
 import styles from "../../pages/ReviewDetail/ReviewDetail.module.scss";
 import back from "../../images/back_btn.png";
 import close from "../../images/close_btn.png";
+import { Link } from "react-router-dom";
 
 const ReviewDetail = () => {
   return (
     <div className={styles.detailHeader}>
-      <img src={back} className={styles.icon} alt="back icon" />
+      <Link to="/">
+        <img src={back} className={styles.icon} alt="back icon" />
+      </Link>
       <span>리뷰 상세보기</span>
-      <img src={close} className={styles.icon} alt="close icon" />
+      <Link to="/">
+        <img src={close} className={styles.icon} alt="close icon" />
+      </Link>
     </div>
   );
 };
